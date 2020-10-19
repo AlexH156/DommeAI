195ce85cc44f5ce7f86deb3377bc59be84d91404
AlexH156 <alexander.huster@web.de>
2020-10-19 16:02:22 +0200

    DommeAI prüft jetzt den potenziellen Weg und nimmt den nur, wenn der frei ist, außerdem kann er sich die Sprünge in jeder sechsten Runde zu Nutze machen (erfolgreich getestet)

--- example.py
+++ example.py
@@ -41,6 +41,7 @@ async def play():
 
     async with websockets.connect(f"{url}?key={key}") as websocket:
         print("Waiting for initial state...", flush=True)
+        counter = 0
         while True:
             state_json = await websocket.recv()
             state = json.loads(state_json)
@@ -49,52 +50,122 @@ async def play():
             if not state["running"] or not own_player["active"]:
                 break
             valid_actions = []
+            counter += 1
 
             #check-speedup
             if own_player["speed"] < 10:
                 newpos = getnewpos(own_player["x"], own_player["y"], own_player["speed"]+1, own_player["direction"])
-                if ([state["height"] - 1, state["width"] - 1] >= newpos) & (
-                        newpos >= [0, 0]):  # Prüfe ob er das Spielfeld verlassen würde
-                    newx = newpos[1]
-                    newy = newpos[0]
-                    if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an der neuen Stelle ist
-                        valid_actions += ["speed_up"]
+                newx = newpos[1]
+                newy = newpos[0]
+                if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newx >= 0) & (newy >= 0):  # Prüfe ob er das Spielfeld verlassen würde
+                    if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an den neuen Stelle sind
+                        hilfe = 0
+                        for i in range(1,own_player["speed"]+1):
+                            if counter % 6 == 0:         # Prüfe ob sechste runde und dann prüfe nicht Lücke (verschwenderisch)
+                                i = 1
+                            newpos = getnewpos(own_player["x"], own_player["y"], i,
+                                               own_player["direction"])
+                            newx = newpos[1]
+                            newy = newpos[0]
+                            if not state["cells"][newy][newx] == 0:
+                                hilfe = 1
+                        if hilfe == 0:
+                            valid_actions += ["speed_up"]
 
-            #check-speeddown
+
+            #check-slowdown
             if own_player["speed"] > 1:
                 newpos = getnewpos(own_player["x"], own_player["y"], own_player["speed"]-1, own_player["direction"])
-                if ([state["height"] - 1, state["width"] - 1] >= newpos) & (
-                        newpos >= [0, 0]):  # Prüfe ob er das Spielfeld verlassen würde
-                    newx = newpos[1]
-                    newy = newpos[0]
-                    if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an der neuen Stelle ist
-                        valid_actions += ["slow_down"]
+                newx = newpos[1]
+                newy = newpos[0]
+                if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newx >= 0) & (newy >= 0):  # Prüfe ob er das Spielfeld verlassen würde
+                    if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an den neuen Stelle sind
+                        if own_player["speed"] > 2:
+                            hilfe = 0
+                            for i in range(1,own_player["speed"]-1):
+                                if counter % 6 == 0:  # Prüfe ob sechste runde und dann prüfe nicht Lücke (verschwenderisch)
+                                    i = 1
+                                newpos = getnewpos(own_player["x"], own_player["y"], i,
+                                                   own_player["direction"])
+                                newx = newpos[1]
+                                newy = newpos[0]
+                                if not state["cells"][newy][newx] == 0:
+                                    hilfe = 1
+                            if hilfe == 0:
+                                valid_actions += ["slow_down"]
+                        else:
+                            valid_actions += ["slow_down"]
 
             # check-nothing
             newpos = getnewpos(own_player["x"], own_player["y"], own_player["speed"], own_player["direction"])
-            if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newpos >= [0, 0]):  # Prüfe ob er das Spielfeld verlassen würde
-                newx = newpos[1]
-                newy = newpos[0]
-                if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an der neuen Stelle ist
-                    valid_actions += ["change_nothing"]
+            newx = newpos[1]
+            newy = newpos[0]
+            if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newx >= 0) & (
+                    newy >= 0):  # Prüfe ob er das Spielfeld verlassen würde
+                if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an den neuen Stelle sind
+                    if own_player["speed"] > 1:
+                        hilfe = 0
+                        for i in range(1, own_player["speed"]):
+                            if counter % 6 == 0:         # Prüfe ob sechste runde und dann prüfe nicht Lücke (verschwenderisch)
+                                i = 1
+                            newpos = getnewpos(own_player["x"], own_player["y"], i,
+                                               own_player["direction"])
+                            newx = newpos[1]
+                            newy = newpos[0]
+                            if not state["cells"][newy][newx] == 0:
+                                hilfe = 1
+                        if hilfe == 0:
+                            valid_actions += ["change_nothing"]
+                    else:
+                        valid_actions += ["change_nothing"]
 
             # check-left
             newpos = getnewpos(own_player["x"], own_player["y"], own_player["speed"],
                                getnewdirection(own_player["direction"], "left"))
-            if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newpos >= [0, 0]):  # Prüfe auf Spielrand verlassen wenn nach links
-                newx = newpos[1]
-                newy = newpos[0]
-                if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an der neuen Stelle ist
-                    valid_actions += ["turn_left"]
+            newx = newpos[1]
+            newy = newpos[0]
+            if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newx >= 0) & (
+                    newy >= 0):  # Prüfe ob er das Spielfeld verlassen würde
+                if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an den neuen Stelle sind
+                    if own_player["speed"] > 1:
+                        hilfe = 0
+                        for i in range(1, own_player["speed"]):
+                            if counter % 6 == 0:         # Prüfe ob sechste runde und dann prüfe nicht Lücke (verschwenderisch)
+                                i = 1
+                            newpos = getnewpos(own_player["x"], own_player["y"], i,
+                                               getnewdirection(own_player["direction"], "left"))
+                            newx = newpos[1]
+                            newy = newpos[0]
+                            if not state["cells"][newy][newx] == 0:
+                                hilfe = 1
+                        if hilfe == 0:
+                            valid_actions += ["turn_left"]
+                    else:
+                        valid_actions += ["turn_left"]
 
             # check-right
             newpos = getnewpos(own_player["x"], own_player["y"], own_player["speed"],
                                getnewdirection(own_player["direction"], "right"))
-            if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newpos >= [0, 0]):  # Prüfe auf Spielrand verlassen wenn nach rechts
-                newx = newpos[1]
-                newy = newpos[0]
-                if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an der neuen Stelle ist
-                    valid_actions += ["turn_right"]
+            newx = newpos[1]
+            newy = newpos[0]
+            if ([state["height"] - 1, state["width"] - 1] >= newpos) & (newx >= 0) & (
+                    newy >= 0):  # Prüfe ob er das Spielfeld verlassen würde
+                if state["cells"][newy][newx] == 0:  # Prüfe ob Schlange an den neuen Stelle sind
+                    if own_player["speed"] > 1:
+                        hilfe = 0
+                        for i in range(1, own_player["speed"]):
+                            if counter % 6 == 0:         # Prüfe ob sechste runde und dann prüfe nicht Lücke (verschwenderisch)
+                                i = 1
+                            newpos = getnewpos(own_player["x"], own_player["y"], i,
+                                               getnewdirection(own_player["direction"], "right"))
+                            newx = newpos[1]
+                            newy = newpos[0]
+                            if not state["cells"][newy][newx] == 0:
+                                hilfe = 1
+                        if hilfe == 0:
+                            valid_actions += ["turn_right"]
+                    else:
+                        valid_actions += ["turn_right"]
 
             action = random.choice(valid_actions)
             print(">", action)
